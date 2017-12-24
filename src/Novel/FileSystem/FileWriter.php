fd40ac422b537042dcf4eceb3a53b4e2f5108398
Marina Sherstuk <marinas@commpeak.com>
2017-12-24 11:04:15 +0200

    Added error handling in FileWriter

--- src/Novel/FileSystem/FileWriter.php
+++ src/Novel/FileSystem/FileWriter.php
@@ -4,17 +4,34 @@ namespace Novel\FileSystem;
 
 use Novel\FileSystem\Exceptions\MkDirPermissionDeniedException;
 use Novel\FileSystem\Exceptions\MkDirUnexpectedExceptionOccurredException;
+use Novel\FileSystem\Exceptions\PathEmptyException;
+use Novel\FileSystem\Exceptions\WriteToFilePermissionDeniedException;
+use Novel\FileSystem\Exceptions\WriteToFileUnexpectedExceptionOccurredException;
 
 
 class FileWriter
 {
 	public static function write(string $path, string $content): void
 	{
+		if (!$path)
+			throw new PathEmptyException();
+		
 		$parts = explode('/', $path);
 		array_pop($parts);
 		self::mkDirRecursively($parts);
 		
-		file_put_contents($path, $content);
+		try
+		{
+			if (file_put_contents($path, $content) === false)
+				throw new WriteToFileUnexpectedExceptionOccurredException($path);
+		}
+		catch (\Throwable $exception)
+		{
+			if (strpos($exception->getMessage(), 'Permission denied') !== false)
+				throw new WriteToFilePermissionDeniedException($path);
+			
+			throw $exception;
+		}
 	}
 	
 	public static function mkDirRecursively(array $dirs): int
