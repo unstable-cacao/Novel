1c141ada3447539c310fa3d4df315cdc1f719e5f
Marina Sherstuk <marinas@commpeak.com>
2018-01-22 08:33:57 +0200

    Started new line middleware

--- src/Novel/Transformation/Utils/StatementNewLineMiddlewareTransform.php
+++ src/Novel/Transformation/Utils/StatementNewLineMiddlewareTransform.php
@@ -4,7 +4,10 @@ namespace Novel\Transformation\Utils;
 
 use Novel\Core\IToken;
 use Novel\Core\Stream\ITokenTransformStream;
+use Novel\Core\Tokens\Abstraction\Statements\IStatementToken;
+use Novel\Core\Tokens\Scope\IScopeToken;
 use Novel\Core\Transforming\ITokenMiddlewareTransform;
+use Novel\Symbols\WhiteSpace\NewLineSymbol;
 
 
 class StatementNewLineMiddlewareTransform implements ITokenMiddlewareTransform
@@ -12,5 +15,8 @@ class StatementNewLineMiddlewareTransform implements ITokenMiddlewareTransform
 	public function executeTransform(IToken $token, ITokenTransformStream $writer, callable $next): void
 	{
 		$writer->push($next());
+		
+		if ($token->parent() instanceof IScopeToken && $token instanceof IStatementToken)
+			$writer->push(NewLineSymbol::class);
 	}
 }
